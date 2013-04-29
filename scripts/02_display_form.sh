345e8234e58da094018f351f029d2da68b706740
Arnaud Kervern <akervern@nuxeo.com>
2013-04-29 15:32:40 +0200

    Fix scripts

--- scripts/02_display_form.sh
+++ scripts/02_display_form.sh
@@ -1,5 +1,5 @@
-$NUXEO_URL=http://localhost:8080
-$LOGIN=Administrator
-$PWD=Administrator
+NUXEO_URL=http://localhost:8080
+LOGIN=Administrator
+PWD=Administrator
 
 curl -i -u $LOGIN:$PWD $NUXEO_URL/nuxeo/site/scenari/importscreen/default/$1
--- scripts/03_move_archive.sh
+++ scripts/03_move_archive.sh
@@ -1,5 +1,5 @@
-$NUXEO_URL=http://localhost:8080
-$LOGIN=Administrator
-$PWD=Administrator
+NUXEO_URL=http://localhost:8080
+LOGIN=Administrator
+PWD=Administrator
 
 curl -i -X POST -d "workspaceRef=$2" -u $LOGIN:$PWD $NUXEO_URL/nuxeo/site/scenari/importscreen/default/$1
